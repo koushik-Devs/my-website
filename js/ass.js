abe795393da8f254b11f482e6c988900b86f1734
koushik-Devs <koushikmondal.me@outlook.com>
2024-08-27 15:07:52 +0530

    Add files via upload

---
+++ js/ass.js
@@ -0,0 +1,6 @@
+  function viewAssignment() {
+            // Add your logic to view the assignment here
+            alert('View Assignment button clicked!');
+            // You can navigate to the assignment page using:
+            window.location.href = 'ass_1.html';
+        };
